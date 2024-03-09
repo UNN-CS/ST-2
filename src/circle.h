93cf03deeb4f69457376a4890ef1028762f88fcf
ISK7 <105171814+ISK7@users.noreply.github.com>
2024-03-09 19:35:12 +0300

    Task 2
    Add task

---
+++ src/circle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<cmath>
+
+class circle
+{
+private:
+    double radius;
+    double ferense;
+    double area;
+public:
+    void setRadius(double rad);
+    void setFerense(double fer);
+    void setArea(double ar);
+    double getRadius();
+    double getFerense();
+    double getArea();
+};
+
---
+++ src/tasks.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"circle.h"
+
+class tasks
+{
+public:
+	double earth();
+	double pool();
+};
+
