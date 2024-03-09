4711a9f9280b50587c87600a49afdc453185237d
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-03-10 01:55:43 +0300

    Задачи и тесты к ним

---
+++ include/tasks.h
@@ -0,0 +1,15 @@
+// Copyright 2024 Kriseev Mikhail, UNN
+
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+#include <utility>
+
+double ropeAroundPlanetTask(double planetRadius, double radiusIncrement);
+double pathAroundPoolAndFencePriceTask(
+        double poolRadius,
+        double pathWidth,
+        double concretePrice,
+        double fencePrice);
+
+#endif //INCLUDE_TASKS_H_
--- test/tests.cpp
+++ test/tests.cpp
@@ -3,65 +3,69 @@
 #include <cstdint>
 #include <cmath>
 #include "circle.h"
+#include "tasks.h"
+
+const double EPS = 1.0e-9;
 
 TEST(CIRCLE_CLASS, getRadius_correct) {
     Circle circle(1.0);
-    EXPECT_NEAR(circle.getRadius(), 1.0, DBL_EPSILON);
+    EXPECT_NEAR(circle.getRadius(), 1.0, EPS);
 }
 TEST(CIRCLE_CLASS, getArea_correct) {
     Circle circle(1.0);
-    EXPECT_NEAR(circle.getArea(), M_PI, DBL_EPSILON);
+    EXPECT_NEAR(circle.getArea(), M_PI, EPS);
 }
 TEST(CIRCLE_CLASS, getFerence_correct) {
     Circle circle(1.0);
-    EXPECT_NEAR(circle.getFerence(), M_PI * 2, DBL_EPSILON);
+    EXPECT_NEAR(circle.getFerence(), M_PI * 2, EPS);
 }
 
 TEST(CIRCLE_CLASS, setRadius_correct_radius) {
     Circle circle(1.0);
     circle.setRadius(2.0);
-    EXPECT_NEAR(circle.getRadius(), 2.0, DBL_EPSILON);
+    EXPECT_NEAR(circle.getRadius(), 2.0, EPS);
 }
 TEST(CIRCLE_CLASS, setRadius_correct_area) {
     Circle circle(1.0);
     circle.setRadius(2.0);
-    EXPECT_NEAR(circle.getArea(), 4.0 * M_PI, DBL_EPSILON);
+    EXPECT_NEAR(circle.getArea(), 4.0 * M_PI, EPS);
 }
 TEST(CIRCLE_CLASS, setRadius_correct_ference) {
     Circle circle(1.0);
     circle.setRadius(2.0);
-    EXPECT_NEAR(circle.getFerence(), 4 * M_PI, DBL_EPSILON);
+    EXPECT_NEAR(circle.getFerence(), 4 * M_PI, EPS);
 }
 TEST(CIRCLE_CLASS, setArea_correct_area) {
     Circle circle(1.0);
     circle.setArea(4 * M_PI);
-    EXPECT_NEAR(circle.getArea(), 4 * M_PI, DBL_EPSILON);
+    EXPECT_NEAR(circle.getArea(), 4 * M_PI, EPS);
 }
 TEST(CIRCLE_CLASS, setArea_correct_radius) {
     Circle circle(1.0);
     circle.setArea(4 * M_PI);
-    EXPECT_NEAR(circle.getRadius(), 2.0, DBL_EPSILON);
+    EXPECT_NEAR(circle.getRadius(), 2.0, EPS);
 }
 TEST(CIRCLE_CLASS, setArea_correct_ference) {
     Circle circle(1.0);
     circle.setArea(4 * M_PI);
-    EXPECT_NEAR(circle.getFerence(), 4 * M_PI, DBL_EPSILON);
+    EXPECT_NEAR(circle.getFerence(), 4 * M_PI, EPS);
 }
 TEST(CIRCLE_CLASS, setFerence_correct_ference) {
     Circle circle(1.0);
     circle.setFerence(4 * M_PI);
-    EXPECT_NEAR(circle.getFerence(), 4 * M_PI, DBL_EPSILON);
+    EXPECT_NEAR(circle.getFerence(), 4 * M_PI, EPS);
 }
 TEST(CIRCLE_CLASS, setFerence_correct_radius) {
     Circle circle(1.0);
     circle.setFerence(4 * M_PI);
-    EXPECT_NEAR(circle.getRadius(), 2.0, DBL_EPSILON);
+    EXPECT_NEAR(circle.getRadius(), 2.0, EPS);
 }
 TEST(CIRCLE_CLASS, setFerence_correct_area) {
     Circle circle(1.0);
     circle.setFerence(4 * M_PI);
-    EXPECT_NEAR(circle.getArea(), 4 * M_PI, DBL_EPSILON);
+    EXPECT_NEAR(circle.getArea(), 4 * M_PI, EPS);
 }
+
 TEST(CIRCLE_CLASS, negative_constructor_arg) {
     EXPECT_ANY_THROW(Circle(-1.0));
 }
@@ -77,4 +81,30 @@ TEST(CIRCLE_CLASS, setArea_negative_input) {
 TEST(CIRCLE_CLASS, setFerence_negative_input) {
     Circle circle(1.0);
     EXPECT_ANY_THROW(circle.setFerence(-1.0));
+}
+TEST(CIRCLE_CLASS, zero_constructor_arg) {
+    EXPECT_ANY_THROW(Circle(0.0));
+}
+
+TEST(CIRCLE_CLASS, setRadius_zero_input) {
+    Circle circle(1.0);
+    EXPECT_ANY_THROW(circle.setRadius(0.0));
+}
+TEST(CIRCLE_CLASS, setArea_zero_input) {
+    Circle circle(1.0);
+    EXPECT_ANY_THROW(circle.setArea(0.0));
+}
+TEST(CIRCLE_CLASS, setFerence_zero_input) {
+    Circle circle(1.0);
+    EXPECT_ANY_THROW(circle.setFerence(0.0));
+}
+
+TEST(TASKS, ropeAroundPlanetTask_Earth_1m) {
+    EXPECT_NEAR(ropeAroundPlanetTask(6371000.0, 1.0), M_1_PI * 0.5, EPS);
+}
+TEST(TASKS, pathAroundPoolAndFencePriceTask) {
+    EXPECT_NEAR(
+            pathAroundPoolAndFencePriceTask(3.0, 1.0, 1000.0, 2000.0),
+            23000.0 * M_PI,
+            EPS);
 }
