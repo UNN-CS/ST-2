d0e8491b90957090f63cc10e444b37c0a625388e
Иван Садиков <sadikov.04@list.ru>
2025-02-26 14:36:05 +0300

    increase tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -59,7 +59,7 @@ TEST(circle_Task, testCircleCreate9) {
   constexpr auto kEpsilon = 0.001;
   constexpr auto kRadius = 0.1;
   constexpr auto kExpectedFence = 0.6283;
-  constexpr auto kExpectedArea = 0.314;
+  constexpr auto kExpectedArea = 0.31415;
   Circle circle(kRadius);
   EXPECT_NEAR(kExpectedFence, circle.GetFerence(), kEpsilon);
   EXPECT_NEAR(kExpectedArea, circle.GetArea(), kEpsilon);
@@ -93,6 +93,6 @@ TEST(circle_Task, testTask1) {
 
 TEST(circle_Task, testTask2) {
   constexpr auto kEpsilon = 0.001;
-  auto kExpectedNumber = 72256.6;
+  auto kExpectedNumber = 72256.6297;
   EXPECT_NEAR(kExpectedNumber, GetSwimingPoolBuildingCost(), kEpsilon);
 }
