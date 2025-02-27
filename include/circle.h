fbda3daae22b0626be50dbd98f513d6792221442
Vladislav Ermolaev <71265338+ermolaevv@users.noreply.github.com>
2025-02-27 21:33:33 +0300

    upload

--- include/circle.h
+++ include/circle.h
@@ -3,5 +3,26 @@
 #define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
+class Circle {
+ public:
+  explicit Circle(double radius);
 
-#endif  // INCLUDE_CIRCLE_H_
+  void setRadius(double radius);
+  void setCircumference(double circumference);
+  void setArea(double area);
+
+  double getRadius() const;
+  double getCircumference() const;
+  double getArea() const;
+
+ private:
+  double radius;
+  double circumference;
+  double area;
+
+  void calculateFromRadius();
+  void calculateFromCircumference();
+  void calculateFromArea();
+};
+
+#endif // INCLUDE_CIRCLE_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,63 @@
 // Copyright 2022 UNN-CS
-#include <cstdint>
+#define _USE_MATH_DEFINES
+
 #include "circle.h"
 
+#include <cmath>
+#include <cstdint>
+#include <stdexcept>
+
+Circle::Circle(double radius) : radius(radius) {
+  if (radius < 0) {
+    throw std::invalid_argument("Radius cannot be negative");
+  }
+  calculateFromRadius();
+}
+
+void Circle::calculateFromRadius() {
+  circumference = 2 * M_PI * radius;
+  area = M_PI * radius * radius;
+}
+
+void Circle::calculateFromCircumference() {
+  radius = circumference / (2 * M_PI);
+  area = M_PI * radius * radius;
+}
+
+void Circle::calculateFromArea() {
+  radius = std::sqrt(area / M_PI);
+  circumference = 2 * M_PI * radius;
+}
+
+void Circle::setRadius(double radius) {
+  if (radius < 0) {
+    throw std::invalid_argument("Radius cannot be negative");
+  }
+
+  this->radius = radius;
+  calculateFromRadius();
+}
+
+void Circle::setCircumference(double circumference) {
+  if (circumference < 0) {
+    throw std::invalid_argument("Circumference cannot be negative");
+  }
+
+  this->circumference = circumference;
+  calculateFromCircumference();
+}
+
+void Circle::setArea(double area) {
+  if (area < 0) {
+    throw std::invalid_argument("Area cannot be negative");
+  }
+
+  this->area = area;
+  calculateFromArea();
+}
+
+double Circle::getRadius() const { return radius; }
+
+double Circle::getCircumference() const { return circumference; }
+
+double Circle::getArea() const { return area; }
