2be4781c6f5b39c5e429ce77d01af7a949ac9a4f
Hand-less <salaev_09@mail.ru>
2025-02-27 15:43:06 +0300

    lab 2

---
+++ include/tasks.h
@@ -0,0 +1,9 @@
+// Copyright 2025 Salaev Vladislav
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+double calculateGapAfterAddingRope(double initialRadius, double addedLength);
+double calculateCostForPool(double poolRadius,
+	double pathWidth, double concreteCost, double fenceCost);
+
+#endif  // INCLUDE_TASKS_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,56 @@
-// Copyright 2022 UNN-CS
-#include <cstdint>
+// Copyright 2025 Salaev Vladislav
 #include "circle.h"
+#include <cmath>
 
+#define PI 3.14159265358979323846
+
+Circle::Circle(double r) : radius(r) {
+    updateFerence();
+    updateArea();
+}
+
+void Circle::setRadius(double r) {
+    radius = r;
+    updateFerence();
+    updateArea();
+}
+
+void Circle::setFerence(double f) {
+    ference = f;
+    updateRadiusFromFerence();
+    updateArea();
+}
+
+void Circle::setArea(double a) {
+    area = a;
+    updateRadiusFromArea();
+    updateFerence();
+}
+
+double Circle::getRadius() const {
+    return radius;
+}
+
+double Circle::getFerence() const {
+    return ference;
+}
+
+double Circle::getArea() const {
+    return area;
+}
+
+void Circle::updateFerence() {
+    ference = 2 * PI * radius;
+}
+
+void Circle::updateArea() {
+    area = PI * radius * radius;
+}
+
+void Circle::updateRadiusFromFerence() {
+    radius = ference / (2 * PI);
+}
+
+void Circle::updateRadiusFromArea() {
+    radius = std::sqrt(area / PI);
+}
