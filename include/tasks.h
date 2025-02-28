b68036c334f42c26bfeb9891f6728fba82ff147b
ovVrLFg8ks <oturin.alexandr@gmail.com>
2025-02-28 16:05:22 +0300

    added circle class, tasks, tests

---
+++ include/tasks.h
@@ -0,0 +1,11 @@
+// Copyright 2025 ovVrLFg8ks
+
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+double ropeTask(double earthRadius, double gap);
+
+double poolTask(double poolRadius, double pathWidth, double concreteSquareCost,
+                double fenceMeterCost);
+
+#endif  // INCLUDE_TASKS_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,33 @@
-// Copyright 2022 UNN-CS
-#include <cstdint>
+// Copyright 2025 ovVrLFg8ks
+
 #include "circle.h"
 
+void Circle::setRadius(double r) {
+  if (r < 0)
+    throw std::invalid_argument("the argument must have a non-negative value");
+  radius = r;
+  ference = 2 * M_PI * r;
+  area = M_PI * r * r;
+}
+
+void Circle::setFerence(double f) {
+  if (f < 0)
+    throw std::invalid_argument("the argument must have a non-negative value");
+  ference = f;
+  radius = f / (2 * M_PI);
+  area = M_PI * radius * radius;
+}
+
+void Circle::setArea(double a) {
+  if (a < 0)
+    throw std::invalid_argument("the argument must have a non-negative value");
+  area = a;
+  radius = std::sqrt(a / M_PI);
+  ference = 2 * M_PI * radius;
+}
+
+double Circle::getRadius() { return radius; }
+
+double Circle::getFerence() { return ference; }
+
+double Circle::getArea() { return area; }
