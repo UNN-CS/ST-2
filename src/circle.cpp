b03f77c901326277a8bfacdadd56ef8a1a5007f6
shvedovav <112872669+shvedovav@users.noreply.github.com>
2025-02-28 18:00:46 +0300

    ST-2 (v1)

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,36 @@
-// Copyright 2022 UNN-CS
+// Copyright 2025 shvedovav
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+
+#include <cmath>
 #include <cstdint>
 #include "circle.h"
 
+Circle::Circle(double r) {
+  setRadius(r);
+}
+void Circle::setRadius(double r) {
+  radius = r;
+  ference = 2.0 * M_PI * r;
+  area = M_PI * r * r;
+}
+void Circle::setFerence(double c) {
+  ference = c;
+  radius = c / (2.0 * M_PI);
+  area = M_PI * radius * radius;
+}
+void Circle::setArea(double a) {
+  area = a;
+  radius = std::sqrt(a / M_PI);
+  ference = 2.0 * M_PI * radius;
+}
+double Circle::getRadius() const {
+  return radius;
+}
+double Circle::getFerence() const {
+  return ference;
+}
+double Circle::getArea() const {
+  return area;
+}
---
+++ src/tasks.cpp
@@ -0,0 +1,33 @@
+// Copyright 2025 shvedovav
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+
+#include "tasks.h"
+#include "circle.h"
+#include <cmath>
+#include <cstdint>
+
+double ropeGapTask() {
+  const double earthKm = 6378.1;
+  const double earthRadiusM = earthKm * 1000.0;
+  Circle earth(earthRadiusM);
+  double oldFerence = earth.getFerence();
+  earth.setFerence(oldFerence + 1.0);
+  double gap = earth.getRadius() - earthRadiusM;
+  return gap;
+}
+
+double poolCostTask() {
+  const double radiusPool = 3.0;
+  const double radiusTotal = 4.0;
+  Circle pool(radiusPool);
+  Circle poolWithPath(radiusTotal);
+  double areaPool = pool.getArea();
+  double areaPoolWithPath = poolWithPath.getArea();
+  double pathArea = areaPoolWithPath - areaPool;
+  double concreteCost = pathArea * 1000.0;
+  double fenceCost = poolWithPath.getFerence() * 2000.0;
+  double totalCost = concreteCost + fenceCost;
+  return totalCost;
+}
