caf766e8a8ed6af4b181be34f100823e5feba4be
Artem Tyshkevich <117100185+artich02@users.noreply.github.com>
2025-03-02 01:00:04 +0300

    fix tasks.h

--- include/tasks.h
+++ include/tasks.h
@@ -1,8 +1,8 @@
 // Copyright 2025 Artem Tyshkevich
-#ifndef INCLUDE_TASK_H_
-#define INCLUDE_TASK_H_
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
 
 double calculateGap();
 double calculatePoolCost();
 
-#endif // INCLUDE_TASK_H_
+#endif // INCLUDE_TASKS_H_
