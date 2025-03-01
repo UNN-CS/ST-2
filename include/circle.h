1b843e6df68233f528ad79d33a2e09f96034b2e8
илья константинов <ilyukon2020@gmail.com>
2025-03-01 16:32:59 +0300

    fix 2

--- include/circle.h
+++ include/circle.h
@@ -4,15 +4,14 @@
 #include <cstdint>
 
 class Circle {
-	private:
+  private:
 		double radius;
 		double ference;
 		double area;
-
 		void UpdateFromRadius();
 		void UpdateFromFerence();
 		void UpdateFromArea();
-	public:
+  public:
 		explicit Circle(double r);
 		void SetRadius(double r);
 		void SetFerence(double f);
@@ -21,5 +20,4 @@ class Circle {
 		double GetFerence();
 		double GetArea();
 };
-
 #endif // INCLUDE_CIRCLE_H_
