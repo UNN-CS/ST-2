4b655096365567e3a49f9bc659b4c1ad52615c96
Ksu04 <kseniyaichetovkina@gmail.com>
2025-03-01 23:55:29 +0300

    v6

--- include/circle.h
+++ include/circle.h
@@ -7,17 +7,18 @@
 const double PI = 3.14159265358979323846264;
 
 class Circle {
-    private:
-    double radius;
-    double ference;
-    double area;
-    public:
-    void setRadius(double _r);
-    void setFerence(double _f);
-    void setArea(double _a);
-    double getRadius();
-    double getFerence();
-    double getArea();
-};
+private:
+ double radius;
+ double ference;
+ double area;
+   
+public:
+ void setRadius(double r);
+ void setFerence(double c);
+ void setArea(double a);
+ double getRadius();
+ double getFerence();
+ double getArea();
+   };
 
 #endif  // INCLUDE_CIRCLE_H_
