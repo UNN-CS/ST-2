86ac48bef88437802e5d76d445dba360ee2d2ab0
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2025-03-03 21:07:37 +0300

    first commit

---
+++ include/tasks.h
@@ -0,0 +1,5 @@
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+double calculateGap();
+double calculatePoolCost();
+#endif  // INCLUDE_TASKS_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,48 @@
 // Copyright 2022 UNN-CS
+#define _USE_MATH_DEFINES
 #include <cstdint>
 #include "circle.h"
+#include <cmath>
 
+
+Circle::Circle(double r) {
+	setRadius(r);
+}
+
+void Circle::setRadius(double r) {
+	radius = r;
+	updateFerence();
+	updateArea();
+}
+
+void Circle::setFerence(double f) {
+	ference = f;
+	radius = f / (2 * M_PI);
+	updateArea();
+}
+
+void Circle::setArea(double a) {
+	area = a;
+	radius = sqrt(a / M_PI);
+	updateFerence();
+}
+
+double Circle::getRadius() const {
+	return radius;
+}
+
+double Circle::getFerence() const {
+	return ference;
+}
+
+double Circle::getArea() const {
+	return area;
+}
+
+void Circle::updateFerence() {
+	ference = 2 * M_PI * radius;
+}
+
+void Circle::updateArea() {
+	area = M_PI * radius * radius;
+}
