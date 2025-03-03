db1117268504e2b7ed2b9ff8346fcc7c3a753f84
Semyon1104 <129722895+Semyon1104@users.noreply.github.com>
2025-03-03 20:00:50 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -47,6 +47,29 @@ TEST(CircleTest, LargeRadius) {
   EXPECT_DOUBLE_EQ(c.getArea(), PI * 1e6 * 1e6);
 }
 
+TEST(CircleTest, NegativeRadius) {
+  Circle c(-5.0);
+  EXPECT_LT(c.getRadius(), 0);
+}
+
+TEST(CircleTest, SetNegativeRadius) {
+  Circle c(5.0);
+  c.setRadius(-10.0);
+  EXPECT_LT(c.getRadius(), 0);
+}
+
+TEST(CircleTest, SetFerenceZero) {
+  Circle c(5.0);
+  c.setFerence(0);
+  EXPECT_DOUBLE_EQ(c.getRadius(), 0);
+}
+
+TEST(CircleTest, SetAreaZero) {
+  Circle c(5.0);
+  c.setArea(0);
+  EXPECT_DOUBLE_EQ(c.getRadius(), 0);
+}
+
 TEST(RopeTest, SmallIncrease) {
   double gap = calculateRopeGap(6378.1 * 1000, 1);
   EXPECT_NEAR(gap, 1 / (2 * PI), 1e-6);
@@ -55,6 +78,11 @@ TEST(RopeTest, SmallIncrease) {
 TEST(RopeTest, NoIncrease) {
   double gap = calculateRopeGap(6378.1 * 1000, 0);
   EXPECT_DOUBLE_EQ(gap, 0);
+}
+
+TEST(RopeTest, LargeIncrease) {
+  double gap = calculateRopeGap(6378.1 * 1000, 100);
+  EXPECT_GT(gap, 10);
 }
 
 TEST(PoolTest, BasicCalculation) {
@@ -71,8 +99,7 @@ TEST(PoolTest, NoPath) {
 
 TEST(PoolTest, LargePool) {
   double cost = calculatePoolCost(100, 10, 500, 1500);
-  double expectedCost =
-    (PI * (110 * 110 - 100 * 100) * 500) + (2 * PI * 110 * 1500);
+  double expectedCost = (PI * (110 * 110 - 100 * 100) * 500) + (2 * PI * 110 * 1500);
   EXPECT_NEAR(cost, expectedCost, 1e-6);
 }
 
@@ -81,6 +108,20 @@ TEST(PoolTest, ZeroRadius) {
   double expectedCost = (PI * 5 * 5 * 500) + (2 * PI * 5 * 1500);
   EXPECT_NEAR(cost, expectedCost, 1e-6);
 }
+
+TEST(PoolTest, ZeroConstructionCost) {
+  double cost = calculatePoolCost(10, 5, 0, 1500);
+  double expectedCost = 2 * PI * 15 * 1500;
+  EXPECT_NEAR(cost, expectedCost, 1e-6);
+}
+
+TEST(PoolTest, ZeroBorderCost) {
+  double cost = calculatePoolCost(10, 5, 1000, 0);
+  double expectedCost = PI * (15 * 15 - 10 * 10) * 1000;
+  EXPECT_NEAR(cost, expectedCost, 1e-6);
+}
+
+
 
 
 
