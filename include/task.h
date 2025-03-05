8cee5e7f7613124ea1f2ab5ef847127cfd1e2d81
Yaroslav <104485749+Yarik3008@users.noreply.github.com>
2025-03-05 23:48:42 +0300

    Update task.h

--- include/task.h
+++ include/task.h
@@ -1,5 +1,7 @@
-#ifndef TASK_H
-#define TASK_H
+// Copyright 2022 UNN-CS
+#ifndef INCLUDE_TASK_H_
+#define INCLUDE_TASK_H_
+#include <cstdint>
 double calculateGap(double earthRadius, double additionalLength);
 double calculatePoolCost(double poolRadius, double pathWidth);
-#endif // TASK_H
+#endif  // INCLUDE_TASK_H_
